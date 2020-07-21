143b86bfe8a350855b067f963d433d39fe642bde
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2020-07-21 15:16:38 +0300

    Akışların Denetlenmesi konusu eklendi.

--- book-007.py
+++ book-007.py
@@ -1,4 +1,5 @@
 import math
+
 # Akışın Denetlenmesi
 
 # if
@@ -6,13 +7,12 @@ a = 1
 b = 2
 print("a = ", a, "b = ", b)
 if a > b:
-    print(a,"değeri", b,"'den büyüktür")
+    print(a, "değeri", b, "'den büyüktür")
 if a < b:
-    print(a,"değeri", b,"'den küçüktür")
+    print(a, "değeri", b, "'den küçüktür")
 if b > 0:
-    c = math.sqrt(4*a+b**2)
-    print("(4*a+b**2)\'nin karekökü,", c,"\'dir ")
-
+    c = math.sqrt(4 * a + b ** 2)
+    print("(4*a+b**2)\'nin karekökü,", c, "\'dir ")
 
 # if - else
 
@@ -39,3 +39,79 @@ for ad in adlar:
         print("Arkadaşın", ad, "bir İngilizdir.")
     else:
         print(ad, "adında birini tanımıyorum.")
+
+# Döngüler
+
+# For Döngüsü
+liste1 = [99, 98, 97]
+liste2 = [1, 3, 7, 11, "çiçek", "böcek", liste1]
+
+for n in liste2:
+    print(n)
+
+# While Döngüsü
+print("WHILE DÖNGÜSÜ")
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+i = 0
+
+# break ile döngünün kırılması
+print("BREAK KOMUTU")
+while True:
+    print(i)
+    i += 1
+    if i >= 5:
+        break
+
+# return komutu ile döngü kırılması
+# Döngüyü kırmakla kalmaz, içinde bulunduğu fonksiyonu da sonlandırır.
+
+# continue komutu
+# Çalıştığında kendinden sonraki işlemleri atlayıp, döngünün başına dönmeyi sağlar.
+print("CONTINUE KOMUTU")
+# TEK SAYILARI EKRANA BASAN ÖRNEK
+i = 0
+while True:
+    i += 1
+    if i % 2 == 0:
+        continue
+    elif i > 10:
+        break
+    else:
+        print(i)
+
+
+# pass komutu
+# Hiç bir eylemsel işlevi yoktur. Bu komutu henüz kodlarını yazmadığımız fonksiyonları
+# tanımlarken veya herhangi bir işlem yapılmamasının istediğimiz koşullarda kullanmamız mümkün
+
+
+def yeni_fonksiyon():
+    pass
+
+
+yeni_fonksiyon()
+
+# range Fonksiyonu
+
+# range(son)
+# range(ilk, son)
+# range(ilk, son, adım)
+
+l1 = []
+l2 = []
+l3 = []
+for i in range(17):
+    l1.append(i)
+print(l1)
+for i in range(2, 11):
+    l2.append(i)
+print(l2)
+for i in range(5, 24, 3):
+    l3.append(i)
+print(l3)
+
+
